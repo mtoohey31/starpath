6911e8830168d5eeca4967a95acc1e6b945e64b1
Matthew Toohey <contact@mtoohey.com>
2024-01-04 16:41:43 -0500

    move nix overlay entry into ocamlPackages

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,15 +7,17 @@
   };
 
   outputs = { self, nixpkgs, utils }: {
-    overlays.default = final: _: {
-      starpath = final.ocamlPackages.buildDunePackage {
-        pname = "starpath";
-        version = "0.1.0";
-        src = builtins.path { path = ./.; name = "starpath-src"; };
-        minimalOcamlVersion = "4.14.1";
-        duneVersion = "3";
-        checkInputs = [ final.ocamlPackages.ounit2 ];
-      };
+    overlays.default = _: prev: {
+      ocamlPackages = prev.ocamlPackages.overrideScope' (final: _: {
+        starpath = final.buildDunePackage {
+          pname = "starpath";
+          version = "0.1.0";
+          src = builtins.path { path = ./..; name = "starpath-src"; };
+          minimalOcamlVersion = "4.14.1";
+          duneVersion = "3";
+          checkInputs = [ final.ounit2 ];
+        };
+      });
     };
   } // utils.lib.eachDefaultSystem (system:
     let
@@ -23,9 +25,9 @@
         overlays = [ self.overlays.default ];
         inherit system;
       };
-      inherit (pkgs) starpath mkShell ocamlformat ocamlPackages;
-      inherit (ocamlPackages) bisect_ppx dune_3 findlib ocaml ocaml-lsp
-        ounit2 ppxlib;
+      inherit (pkgs) mkShell ocamlformat ocamlPackages;
+      inherit (ocamlPackages) bisect_ppx dune_3 findlib ocaml ocaml-lsp ounit2
+        ppxlib starpath;
     in
     {
       packages.default = starpath;
